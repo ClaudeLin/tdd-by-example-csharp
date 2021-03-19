48668ea584968b27f68bc58f1485e71778c89f0f
ClaudeLin <linc27595@gmail.com>
2021-03-19 22:36:55 +0800

    Ch1:[Refactor] Move class

--- tdd-by-example-the-money-test/MoneyTests.cs
+++ tdd-by-example-the-money-test/MoneyTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using tdd_by_example_the_money;
 
 namespace tdd_by_example_the_money_test
 {
@@ -7,22 +8,9 @@ namespace tdd_by_example_the_money_test
         [Test]
         public void TestMultiplication()
         {
-            Dollar five = new Dollar(5);
+            var five = new Dollar(5);
             five.Times(2);
             Assert.AreEqual(10, five.Amount);
-        }
-    }
-
-    public class Dollar
-    {
-        public int Amount=10;
-
-        public Dollar(int amount)
-        {
-        }
-
-        public void Times(int multiplier)
-        {
         }
     }
 }
---
+++ tdd-by-example-the-money/Dollar.cs
@@ -0,0 +1,15 @@
+﻿namespace tdd_by_example_the_money
+{
+    public class Dollar
+    {
+        public int Amount=10;
+
+        public Dollar(int amount)
+        {
+        }
+
+        public void Times(int multiplier)
+        {
+        }
+    }
+}
