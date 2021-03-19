103471bb038b4c4f982e3feea5121822adb56d8c
ClaudeLin <linc27595@gmail.com>
2021-03-19 23:01:46 +0800

    Ch2:[Test] Define how to use Times(), it should return another dollar object

--- tdd-by-example-the-money-test/MoneyTests.cs
+++ tdd-by-example-the-money-test/MoneyTests.cs
@@ -9,10 +9,11 @@ namespace tdd_by_example_the_money_test
         public void TestMultiplication()
         {
             var five = new Dollar(5);
-            five.Times(2);
+            var product = five.Times(2);
+
             Assert.AreEqual(10, five.Amount);
-            five.Times(3);
-            Assert.AreEqual(15,five.Amount);
+            product = five.Times(3);
+            Assert.AreEqual(15, five.Amount);
         }
     }
 }
