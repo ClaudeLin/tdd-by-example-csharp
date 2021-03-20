e2bd117c036ea4e71a17a46edae984b6948a032e
ClaudeLin <linc27595@gmail.com>
2021-03-21 02:31:44 +0800

    Ch8:[Test] Adjust how to get franc instance from new to Money.Franc

--- tdd-by-example-the-money-test/MoneyTests.cs
+++ tdd-by-example-the-money-test/MoneyTests.cs
@@ -19,18 +19,18 @@ namespace tdd_by_example_the_money_test
             Assert.IsTrue(Money.Dollar(5).Equals(Money.Dollar(5)));
             Assert.IsFalse(Money.Dollar(5).Equals(Money.Dollar(6)));
 
-            Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
-            Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
+            Assert.IsTrue(Money.Franc(5).Equals(Money.Franc(5)));
+            Assert.IsFalse(Money.Franc(5).Equals(Money.Franc(6)));
 
-            Assert.IsFalse(new Franc(5).Equals(Money.Dollar(5)));
+            Assert.IsFalse(Money.Franc(5).Equals(Money.Dollar(5)));
         }
 
         [Test]
         public void TestFrancMultiplication()
         {
-            var five = new Franc(5);
-            Assert.AreEqual(new Franc(10), five.Times(2));
-            Assert.AreEqual(new Franc(15), five.Times(3));
+            Money five = Money.Franc(5);
+            Assert.AreEqual(Money.Franc(10), five.Times(2));
+            Assert.AreEqual(Money.Franc(15), five.Times(3));
         }
     }
 }
