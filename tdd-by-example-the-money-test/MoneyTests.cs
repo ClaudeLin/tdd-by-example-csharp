55f68fb73ab7aa8dedbf3b3aeb3d1bfe95ceaf36
ClaudeLin <linc27595@gmail.com>
2021-03-21 02:05:09 +0800

    Ch8:[Test] Define Dollar how to get Dollar instance

--- tdd-by-example-the-money-test/MoneyTests.cs
+++ tdd-by-example-the-money-test/MoneyTests.cs
@@ -8,7 +8,7 @@ namespace tdd_by_example_the_money_test
         [Test]
         public void TestMultiplication()
         {
-            var five = new Dollar(5);
+            var five = Money.Dollar(5);
             Assert.AreEqual(new Dollar(10), five.Times(2));
             Assert.AreEqual(new Dollar(15), five.Times(3));
         }
@@ -18,10 +18,10 @@ namespace tdd_by_example_the_money_test
         {
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
-            
+
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
-            
+
             Assert.IsFalse(new Franc(5).Equals(new Dollar(5)));
         }
 
