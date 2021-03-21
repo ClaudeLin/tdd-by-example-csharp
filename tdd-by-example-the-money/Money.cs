2a8b0d23869b9f91f5f96e46f30f383959db5f23
ClaudeLin <linc27595@gmail.com>
2021-03-21 15:47:43 +0800

    Ch11:[Refactor] Reformat and apply rider's suggest

--- tdd-by-example-the-money/Money.cs
+++ tdd-by-example-the-money/Money.cs
@@ -2,40 +2,40 @@
 {
     public class Money
     {
-        protected int Amount;
-        protected string _currency;
+        private readonly int _amount;
+        private readonly string _currency;
 
 
-        public Money(int amount, string currency)
+        private Money(int amount, string currency)
         {
-            Amount = amount;
+            _amount = amount;
             _currency = currency;
         }
 
         public Money Times(int multiplier)
         {
-            return new Money(Amount * multiplier, _currency);
+            return new Money(_amount * multiplier, _currency);
         }
 
         public override bool Equals(object? obj)
         {
             var money = (Money) obj;
-            return Amount == money.Amount && _currency.Equals(money._currency);
+            return _amount == money._amount && _currency.Equals(money._currency);
         }
 
         public override string ToString()
         {
-            return Amount + " " + _currency;
+            return _amount + " " + _currency;
         }
 
         public static Money Dollar(int amount)
         {
-            return new Money(amount, "USD");
+            return new(amount, "USD");
         }
 
         public static Money Franc(int amount)
         {
-            return new Money(amount, "CHF");
+            return new(amount, "CHF");
         }
 
         public string Currency()
