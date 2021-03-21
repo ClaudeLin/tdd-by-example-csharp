06738b9a0564c85edbd24c8532774847e8c807b1
ClaudeLin <linc27595@gmail.com>
2021-03-21 16:43:17 +0800

    Ch13:[Refactor] Extract method for prepare move reduce to sum class

--- tdd-by-example-the-money/Bank.cs
+++ tdd-by-example-the-money/Bank.cs
@@ -5,6 +5,12 @@
         public Money Reduce(IExpression source, string to)
         {
             Sum sum = (Sum)source;
+
+            return Reduce(to, sum);
+        }
+
+        private Money Reduce(string to, Sum sum)
+        {
             int amount = sum.Augend._amount + sum.Addend._amount;
             return new Money(amount, to);
         }
