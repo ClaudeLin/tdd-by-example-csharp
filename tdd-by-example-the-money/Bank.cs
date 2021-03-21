3c2d9a90f80949f6579eae6b40e45d73c78d9867
ClaudeLin <linc27595@gmail.com>
2021-03-21 16:52:01 +0800

    Ch13:[Green] Make Test pass by cast type

--- tdd-by-example-the-money/Bank.cs
+++ tdd-by-example-the-money/Bank.cs
@@ -4,6 +4,7 @@
     {
         public Money Reduce(IExpression source, string to)
         {
+            if (source is Money) return (Money) source;
             Sum sum = (Sum) source;
             return sum.Reduce(to);
         }
