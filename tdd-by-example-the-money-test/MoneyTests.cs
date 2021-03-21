9f835c067104aef6d79cf5b6e82cb173578c54ca
ClaudeLin <linc27595@gmail.com>
2021-03-21 15:58:03 +0800

    Ch12:[Experiment] Opposite-down, starting from assertion and backward to find/think where it came from

--- tdd-by-example-the-money-test/MoneyTests.cs
+++ tdd-by-example-the-money-test/MoneyTests.cs
@@ -43,6 +43,7 @@ namespace tdd_by_example_the_money_test
         {
             Money sum = Money.Dollar(5).Plus(Money.Dollar(5));
             Assert.AreEqual(Money.Dollar(10),sum);
+            Assert.AreEqual(Money.Dollar(10),reduced);
         }
     }
 }
