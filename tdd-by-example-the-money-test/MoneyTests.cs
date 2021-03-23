83b0ba729ca275497628477eb57c2db59e704992
CladueLin <linc27595@gmail.com>
2021-03-24 01:04:34 +0800

    Ch14:[Feat] Create Pair class for Bank

--- tdd-by-example-the-money-test/MoneyTests.cs
+++ tdd-by-example-the-money-test/MoneyTests.cs
@@ -83,13 +83,7 @@ namespace tdd_by_example_the_money_test
             var bank = new Bank();
             bank.AddRate("CHF", "USD", 2);
             var result = bank.Reduce(Money.Franc(2), "USD");
-            Assert.AreEqual(Money.Dollar(1),result);
-        }
-
-        [Test]
-        public void TestArrayEquals()
-        {
-            Assert.AreSame(new object[]{"abc"},new object[]{"abc"});
+            Assert.AreEqual(Money.Dollar(1), result);
         }
     }
 }
---
+++ tdd-by-example-the-money/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace tdd_by_example_the_money
+{
+    public class Pair
+    {
+        private string from;
+        private string to;
+
+        public Pair(string from,string to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
